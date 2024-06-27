dc9554270190bb1aad269aab67f3b7d27f1d271e
oxyn <98268537+OxynDev@users.noreply.github.com>
2024-06-28 00:04:20 +0200

    Update append_proxies.py

--- append_proxies.py
+++ append_proxies.py
@@ -7,7 +7,7 @@ new = []
 
 for i in tokens_list:
     new_data = None
-    if i.count("|") == 4:
+    if i.count("|") == 2:
         new_data = i + "|" + random.choice(proxies_list)
     else:
         new_data = i
